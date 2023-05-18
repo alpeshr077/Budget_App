b8a121bee229bb585a42d6bbf560c9c400e8a6a9
Alpesh Rathava <alpeshrathava301@gmail.com>
2023-05-18 08:36:02 +0530

    Commit_task_1

--- app/src/main/java/com/alpesh1/budget_app/Home_Fragment.kt
+++ app/src/main/java/com/alpesh1/budget_app/Home_Fragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class Add_Fragment : Fragment() {
+class Home_Fragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
-
-
-        return inflater.inflate(R.layout.fragment_add_, container, false)
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home_, container, false)
     }
 
 }
--- app/src/main/java/com/alpesh1/budget_app/MainActivity.kt
+++ app/src/main/java/com/alpesh1/budget_app/MainActivity.kt
@@ -2,14 +2,27 @@ package com.alpesh1.budget_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.alpesh1.budget_app.R.id.bt_home
 import com.alpesh1.budget_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        addFragment(Home_Fragment())
+
     }
+
+    private fun addFragment(fragment: Fragment) {
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragpager,fragment).commit()
+    }
+
 }
